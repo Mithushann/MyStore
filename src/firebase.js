1cc68e854321310fcf2d18a114efb4482cc7db2a
Arivu Krishnamoorthy <arivu.krishnamoorthy@knowit.se>
2024-08-29 14:05:51 +0200

    auth0

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    authDomain: process.env.REACT_APP_AUTH0_DOMAIN,
     databaseURL: process.env.REACT_APP_DB_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
